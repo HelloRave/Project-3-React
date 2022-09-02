8806f1f755904fa7a431de9bce737e3707c133f4
HelloRave <huang.weiwei@trentglobal.edu.sg>
2022-09-02 03:59:04 +0000

    Updated API base url

--- project-03/src/api.js
+++ project-03/src/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://8080-hellorave-project3expre-jqbiqm7aypa.ws-us63.gitpod.io/api'
+    baseURL: 'https://hww-tgc18-project-03.herokuapp.com/api'
+    // baseURL: 'https://8080-hellorave-project3expre-jqbiqm7aypa.ws-us63.gitpod.io/api'
 })
